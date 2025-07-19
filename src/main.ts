d2c4cc7da9dff247cf2c1fed59ae897092dcd1f9
Sam <sriram.pant5@gmail.com>
2025-07-19 20:19:30 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from "cookie-parser"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser()); // Enable cookie parser
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // IMPORTANT: Allow cookies to be sent
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
